14af39049c8ec389b6f3331c82b58654d9b4a128
Rakesh Shrestha <rak3sh.shrestha@gmail.com>
2024-12-21 19:49:42 +0545

    feat: adds module-resolver native-icons

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  project: {},
+  'react-native-vector-icons': {
+    platforms: {
+      ios: null,
+    },
+  },
+  assets: ['./src/assets/fonts/'],
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+  getSourceExts() {
+    return ['ts', 'tsx'];
+  },
+};
